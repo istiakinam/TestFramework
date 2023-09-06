4d512fe81f85c3766f6075d3d139caf160b938f7
istiakinam <istiak.inam@gmail.com>
2023-09-06 12:07:30 +0600

    added test input

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -7,3 +7,18 @@ it('has a text input', async () => {
 
     assert(input)
 })
+
+it('shows a success message with valid input', async () => {
+    const dom = await render('index.html')
+
+    const input = dom.window.document.querySelector('input')
+    input.value = 'myemail@gmail.com'
+
+    dom.window.document 
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'))
+
+    const h1 = dom.window.document.querySelector('h1')
+
+    console.log(h1.innerHTML)
+})
