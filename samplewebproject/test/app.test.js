7271d28ccf998e3d9f6e2b004b0941f2cbed81c9
istiakinam <istiak.inam@gmail.com>
2023-09-07 16:51:14 +0600

    added all testing framework

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -20,5 +20,20 @@ it('shows a success message with valid input', async () => {
 
     const h1 = dom.window.document.querySelector('h1')
 
-    console.log(h1.innerHTML)
+    assert.strictEqual(h1.innerHTML, 'Email is good')
+})
+
+it('shows a fail message with invalid input', async () => {
+    const dom = await render('index.html')
+
+    const input = dom.window.document.querySelector('input')
+    input.value = 'myemsdsdfe'
+
+    dom.window.document 
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'))
+
+    const h1 = dom.window.document.querySelector('h1')
+
+    assert.strictEqual(h1.innerHTML, 'Email invalid')
 })
